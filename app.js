a4280ce6ccceba1223b66c38df6c5b2e62842594
Bradyn <bradyn.walsh@gmail.com>
2016-02-25 21:07:18 +1030

    Added due dates
    I think this is the last challenge I will implement

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 var todo = {};
 
 // Todos have a description and a status (done or not)
-// Due Dates will be added at a later date
+// Due Dates will be added at a later date TODO
 todo.Todo = function(data) {
     this.description = m.prop(data.description);
     this.status = m.prop(data.status ? true : false);
     this.dateCreated = m.prop(data.dateCreated ? data.dateCreated : new Date().toDateString());
+    this.dueDate = m.prop(data.dueDate)
 };
 
 // A TodoList is an array of Todo's
@@ -31,6 +32,9 @@ todo.model = (function() {
 
         // Storage of new todo description before it is created
         model.description = m.prop("");
+
+        // Storage of new todo due date before it is created
+        model.dueDate = m.prop("");
 
         // Sorts, updates and stores the list (should be called at the end of every function that changes the list)
         model.updateList = function() {
@@ -86,22 +90,45 @@ todo.model = (function() {
             model.compareFunction = function(task1,task2) {
                 // Convert date string to Dates
                 var date1 = new Date(task1.dateCreated());
-                var date2 = new Date(task1.dateCreated());
+                var date2 = new Date(task2.dateCreated());
 
                 // Compares the dates
                 if (date1 === date2) {
                     return 0; // Returns 0 if dates are equal
+                } else if (date1 > date2) {
+                    return 1; // task1 should be after task2
+                } else {
+                    return -1; // task1 should be before task2
+                }
+            }
+            model.updateList();
+        };
+
+        // Sets the compare function to sort by due date
+        model.sortByDueDate = function() {
+            model.compareFunction = function(task1,task2) {
+                // Convert date strings to Dates
+                var date1 = new Date(task1.dueDate());
+                var date2 = new Date(task2.dueDate());
+
+                // Compares the dates
+                if (date1 === date2) {
+                    return 0; // Return 0 if dates are equal (they stay in the same place)
                 } else {
                     return (date1 > date2 ? 1 : -1); // Returns -1 if task1 should be before task2, 1 otherwise
                 }
             }
-        };
+            model.updateList();
+        }
 
         // Adds a todo to the list, alerts if the description box was empty
         model.addTask = function() {
-            if (model.description()) {
-                model.list.push(new todo.Todo({description: model.description()}));
+            if (model.description() && model.dueDate()) {
+                // date input is a string not a Date, so some conversion is needed
+                model.list.push(new todo.Todo({description: model.description(), dueDate: new Date(model.dueDate()).toDateString()}));
+
                 model.description(""); // Clears the description field
+                model.dueDate("");
                 model.updateList();
             }
         };
@@ -158,13 +185,18 @@ todo.tasksView = function() {
                 m("input[type=button]", {onclick: todo.model.sortByCreationDate,
                                          value: "Date Created"})
             ]),
+            m("th", [
+                m("input[type=button]", {onclick: todo.model.sortByDueDate,
+                                         value: "Due Date"})
+            ]),
           todo.model.list
           .map(function(task) {
               return m("tr", [
                   m("td", [
-                      m("input[type=checkbox]", {onclick: m.withAttr("checked", task.status), checked: task.status()})]),
+                      m("input[type=checkbox]", {onclick: function(){task.status(!task.status()); todo.model.updateList()}, checked: task.status()})]),
                   m("td", {style: {textDecoration: task.status() ? "line-through" : "none" }}, task.description()),
-                  m("td", task.dateCreated().toString())
+                  m("td", task.dateCreated()),
+                  m("td", {style: {color: (Date.parse(task.dueDate()) > Date.now()) ? "green" : "red"}} ,task.dueDate())
               ])
           })
          ]),
@@ -178,6 +210,7 @@ todo.newTaskView = function() {
     return m("div", [
         m("h2", "Add new task"),
         m("input", {onchange: m.withAttr("value", todo.model.description), value: todo.model.description()}),
+        m("input[type=date]", {onchange: m.withAttr("value", todo.model.dueDate), value: todo.model.dueDate()}),
         m("input[type=button]", {onclick: todo.model.addTask,
                                  value: "Add task"})
     ])
